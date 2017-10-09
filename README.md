14cd354d0d9372e7f0de605a19a93b74a46a5b50
sorincovor <sorincovor@yahoo.com>
2017-10-09 23:12:24 +0300

    move faces

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# material-to-texture
-Takes all the colors from a .blend file and creates a texture to be used as a UV map.
+Takes all the colors from a .blend file and creates a texture to be used as a UV map. <br>
 
-Gamma correction:
-https://stackoverflow.com/questions/17910543/convert-gamma-rgb-curves-to-rgb-curves-with-no-gamma/17910665#17910665
+Gamma correction: <br>
+https://stackoverflow.com/questions/17910543/convert-gamma-rgb-curves-to-rgb-curves-with-no-gamma/17910665#17910665 <br>
+
+Useful UV stuff: <br>
+https://blender.stackexchange.com/questions/70676/python-selecting-faces-from-a-material-index <br>
--- unwrap.py
+++ unwrap.py
@@ -1,20 +1,34 @@
 import bpy
 import bmesh
 
+# doesn't work properly with separate meshes
+
+def moveFace():
+	area = bpy.context.area.type
+	bpy.context.area.type = 'IMAGE_EDITOR'
+	bpy.ops.uv.select_all(action = 'TOGGLE')
+	bpy.ops.transform.translate(value = (1, 1, 0), constraint_axis = (False, False, False), constraint_orientation = 'GLOBAL', mirror = False, proportional = 'DISABLED', proportional_edit_falloff = 'SMOOTH', proportional_size = 1)
+	bpy.context.area.type = area
+
 def unwrap(obj):
 	bpy.ops.object.mode_set(mode = 'EDIT')
-	bpy.ops.uv.smart_project(island_margin = 0.4)
+	obj = bpy.context.object
 	me  = obj.data
 	bm = bmesh.from_edit_mesh(me)
 	for face in bm.faces:
-		if face.material_index == index:
+		if face.material_index == 1:
 			face.select = True
-	me.update()
+			# move to the correct position
+			moveFace()
+			face.select = False
 
 for obj in bpy.data.objects:
 	if obj.type == 'MESH':
+		bpy.context.scene.objects.active = obj
+		bpy.ops.mesh.uv_texture_add()
+		bpy.ops.object.editmode_toggle()
+		bpy.ops.uv.smart_project(island_margin = 0.4)
 		unwrap(obj)
-
 
 # for each face in faces
 # smart uv project
