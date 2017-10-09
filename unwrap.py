c752a92bd5d3bc63e16914fc962e7592203a65eb
sorincovor <sorincovor@yahoo.com>
2017-10-09 23:42:18 +0300

    me.update

--- unwrap.py
+++ unwrap.py
@@ -7,22 +7,23 @@ def moveFace():
 	area = bpy.context.area.type
 	bpy.context.area.type = 'IMAGE_EDITOR'
 	bpy.ops.uv.select_all(action = 'TOGGLE')
-	bpy.ops.transform.resize(value = (0.1, 0.1, 0.1), constraint_axis = (False, False, False), constraint_orientation = 'GLOBAL', mirror = False, proportional = 'DISABLED', proportional_edit_falloff = 'SMOOTH', proportional_size = 1)
+	bpy.ops.transform.resize(value=(0.1, 0.1, 0.1), constraint_axis = (False, False, False), constraint_orientation = 'GLOBAL', mirror = False, proportional = 'DISABLED', proportional_edit_falloff = 'SMOOTH', proportional_size = 1)
 	bpy.ops.transform.translate(value = (1, 1, 0), constraint_axis = (False, False, False), constraint_orientation = 'GLOBAL', mirror = False, proportional = 'DISABLED', proportional_edit_falloff = 'SMOOTH', proportional_size = 1)
 	bpy.context.area.type = area
 	bpy.ops.uv.select_all(action = 'TOGGLE')
-
+	
 def unwrap(obj):
-	bpy.ops.object.mode_set(mode = 'EDIT')
-	obj = bpy.context.object
+	bpy.ops.object.mode_set(mode = 'EDIT')         
+	obj = bpy.context.object                  
 	me  = obj.data
-	bm = bmesh.from_edit_mesh(me)
+	bm = bmesh.from_edit_mesh(me)        
 	for face in bm.faces:
 		if face.material_index == 1:
 			face.select = True
 			# move to the correct position
 			moveFace()
 			face.select = False
+		me.update()
 
 for obj in bpy.data.objects:
 	if obj.type == 'MESH':
@@ -31,9 +32,3 @@ for obj in bpy.data.objects:
 		bpy.ops.object.editmode_toggle()
 		bpy.ops.uv.smart_project(island_margin = 0.4)
 		unwrap(obj)
-
-# for each face in faces
-# smart uv project
-# read material index
-# scale (very small)
-# move accordingly
